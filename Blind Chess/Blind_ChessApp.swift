486937675dce4293935bf9f17b1f98246f7fa209
Tyler Hartman <tyhartman@outlook.com>
2026-01-05 13:06:22 -0500

    Initial Commit

---
+++ Blind Chess/Blind_ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blind_ChessApp.swift
+//  Blind Chess
+//
+//  Created by Tyler Hartman on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Blind_ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
